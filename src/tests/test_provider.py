ecdc3918cb0a14325f511d1f7f960da86268c539
phaer <hello@phaer.org>
2023-07-21 16:18:22 +0200

    add requests[socks] integration test

--- src/tests/test_provider.py
+++ src/tests/test_provider.py
@@ -10,20 +10,21 @@ def test_requests(load_case, resolver):
     inputs, expected_lock = load_case("requests-socks")
     requirements = [Requirement(r) for r in inputs.get("requirements")]
     result = resolver.resolve(requirements)
-    assert Identifier("certifi") in result.mapping
-    assert Identifier("PySocks") not in result.mapping
-
+    assert Identifier("requests", ("socks",)) in result.mapping
+    assert Identifier("pysocks") in result.mapping
     assert expected_lock == generate_lock(result)
 
 
-# def test_resolve_requests(resolver):
-#    requirements = [Requirement("requests")]
-#    result = resolver.resolve(requirements)
-#    assert Identifier("certifi") in result.mapping
-#    assert Identifier("PySocks") not in result.mapping
-#
-#
-# def test_resolve_requests_extra(resolver):
-#    requirements = [Requirement("requests[socks]")]
-#    result = resolver.resolve(requirements)
-#    assert Identifier("pysocks") in result.mapping
+def test_requests_without_socks(load_case, resolver):
+    inputs, expected_lock = load_case("requests-socks")
+    requirements = [Requirement("requests")]
+    result = resolver.resolve(requirements)
+    assert Identifier("requests") in result.mapping
+    assert (
+        Identifier(
+            "requests",
+            ("socks"),
+        )
+        not in result.mapping
+    )
+    assert Identifier("pysocks") not in result.mapping
